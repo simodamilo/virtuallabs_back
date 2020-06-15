092f1eb26e3ad501c43b44087173ea07f888fce2
Gianmarco Liaci <liacigianmarco@gmail.com>
2020-06-15 18:57:29 +0200

    First commit

---
+++ src/main/java/it/polito/ai/virtuallabs_back/VirtuallabsBackApplication.java
@@ -0,0 +1,13 @@
+package it.polito.ai.virtuallabs_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuallabsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtuallabsBackApplication.class, args);
+    }
+
+}
